4fa37ced3013c236c74fa9701ca9ba176a753c84
KwanYee Tan <tkyee1998@gmail.com>
2024-06-08 21:13:14 +0800

    added custom tokens for antd modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { ConfigProvider } from "antd";
+
 import CustomRoutes from "./CustomRoutes";
+import { ScheduleProvider } from "./pages/schedule/context-provider";
 
 function App() {
-  return <CustomRoutes />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: "#7ab890",
+          borderRadius: 6,
+
+          // Alias Token
+          colorBgContainer: "none",
+          colorBgMask: "rgba(0, 0, 0, 0.7)",
+        },
+      }}
+    >
+      <ScheduleProvider>
+        <CustomRoutes />
+      </ScheduleProvider>
+    </ConfigProvider>
+  );
 }
 
 export default App;
---
+++ src/mock-data-provider/location.js
@@ -0,0 +1,17 @@
+export const locations = [
+  {
+    id: 1,
+    name: "Location 1",
+    description: "Location 1 description",
+  },
+  {
+    id: 2,
+    name: "Location 2",
+    description: "Location 2 description",
+  },
+  {
+    id: 3,
+    name: "Location 3",
+    description: "Location 3 description",
+  },
+];
